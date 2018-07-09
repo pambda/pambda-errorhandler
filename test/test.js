39a83e924ff33630f27f86ae8551ee13771c9b1c
Kengo Nakatsuka <kengo.nakatsuka@gmail.com>
2018-07-09 20:58:25 +0900

    Modify error responses for local running

--- test/test.js
+++ test/test.js
@@ -2,7 +2,7 @@ const test = require('tape');
 const { errorhandler } = require('..');
 
 test('test', t => {
-  t.plan(2);
+  t.plan(3);
 
   const pambda = errorhandler();
 
@@ -13,5 +13,24 @@ test('test', t => {
   lambda({}, {}, (err, result) => {
     t.error(err);
     t.equal(result.statusCode, 500);
+    t.equal(result.body.stack, undefined);
+  });
+});
+
+test('test for local running', t => {
+  t.plan(3);
+
+  process.env.AWS_SAM_LOCAL = 'true';
+
+  const pambda = errorhandler();
+
+  const lambda = pambda((event, context, callback) => {
+    callback(new Error(''));
+  });
+
+  lambda({}, {}, (err, result) => {
+    t.error(err);
+    t.equal(result.statusCode, 500);
+    t.ok(Array.isArray(result.body.stack));
   });
 });
